14237cd16bfb0059aa234a94fe2bcc732a50bf27
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 20:33:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,13 @@ def long_planeteer_calls (array)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.find do |word|
+    word == cheese_types[0]
+    word == cheese_types[1]
+    word == cheese_types[2]
+  end
 end
