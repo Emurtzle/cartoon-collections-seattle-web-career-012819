719ab21a034ccef2cf105b3582f970a73a4a5ff7
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 20:37:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,5 @@ def find_the_cheese (array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  array.each do |word|
-    word == cheese_types[0] || word == cheese_types[1] || word == cheese_types[2]
-  end
+  
 end
